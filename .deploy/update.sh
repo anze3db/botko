3e54015d304e5daf40302c826fe46a4da4435b09
Anže Pečar <anze@pecar.me>
2024-09-15 02:28:02 +0100

    Fix update script

--- .deploy/update.sh
+++ .deploy/update.sh
@@ -3,8 +3,6 @@ set -e
 pushd "$(dirname "$0")/.."
 git pull
 uv sync --frozen
-uv run --frozen python manage.py collectstatic --noinput
-uv run --frozen python manage.py migrate
 sudo kill -hup `cat gunicorn.pid`
 sudo systemctl restart botko-scheduler
 echo `date "+%Y-%m-%d %H:%M:%S.%3N"` ' Updated' >> update.log
