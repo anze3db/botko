5def5d3b8561778fde6d2647711b7726d56eefb5
Anže Pečar <anze@pecar.me>
2024-11-08 23:42:54 +0000

    Move pid file to /var/run

--- .deploy/update.sh
+++ .deploy/update.sh
@@ -3,7 +3,7 @@ set -e
 pushd "$(dirname "$0")/.."
 git pull
 uv sync --locked
-sudo kill -hup `cat gunicorn.pid`
+sudo kill -hup `cat /var/run/gunicorn-botko.pidd`
 sudo systemctl restart botko-scheduler
 echo `date "+%Y-%m-%d %H:%M:%S.%3N"` ' Updated' >> update.log
 popd
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,4 +3,4 @@ bind = "unix:botko.sock"
 workers = 1
 threads = 4
 proc_name = "botko"
-pidfile = "gunicorn.pid"
+pidfile = "/var/run/gunicorn-botko.pid"
