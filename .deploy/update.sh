a04ed46adaa026f168daee057d5698a3d3ba6ad8
Anže Pečar <anze@pecar.me>
2024-11-10 23:13:53 +0000

    Update deploy settings

--- .deploy/update.sh
+++ .deploy/update.sh
@@ -3,7 +3,7 @@ set -e
 pushd "$(dirname "$0")/.."
 git pull
 uv sync --locked
-ps axf | grep 'gunicorn: master \[botko\]' | awk '{print "sudo kill -hup " $1}' | sh
+sudo systemctl reload botko
 sudo systemctl restart botko-scheduler
 echo `date "+%Y-%m-%d %H:%M:%S.%3N"` ' Updated' >> update.log
 popd
